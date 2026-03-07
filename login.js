b4f0961e060a04e452326cb9b5bd1a347fb7829b
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-03-07 14:23:53 +0600

    added login functionality

---
+++ login.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const signBtn = () => {
+
+    const userInput = document.getElementById('user-input')
+    const userValue = userInput.value;
+    const passInput = document.getElementById('pass-input')
+    const passValue = passInput.value;
+    
+    if (userValue === 'admin' && passValue === 'admin123') {
+      console.log('secces')
+      window.location.assign("./home.html");
+    }else{
+        alert('failed')
+    }
+}
